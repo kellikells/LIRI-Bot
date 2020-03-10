add80cbe1d4e2ba661ba192806c5464c603cb2f1
kelli <kellikells@gmail.com>
2020-03-10 15:14:36 -0700

    'concert-this' and bandsInTown api search is working

--- liri.js
+++ liri.js
@@ -14,41 +14,58 @@ var moment = require('moment');
 
 // =============== GRABBING CL ARGUMENTS ===============
 var searchMethod = process.argv[2];     // use this in switch statement
-var searchTerm = process.argv.slice(3).join("");  // - joins remaining arguments for search
+var searchTerm = process.argv.slice(3).join('');  // - joins remaining arguments for search
 
-console.log("HOW TO SEARCH: " + searchMethod);
-console.log("SEARCH TERM(s): " + searchTerm);
+console.log('HOW TO SEARCH: ' + searchMethod);
+console.log('SEARCH TERM(s): ' + searchTerm);
 
 // =============== SWITCH STATEMENT ===============
 
 switch (searchMethod) {
 
     // --- bands in town
-    case "concert-this":
-        bandsInTownURL = "https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp";
-        console.log("bandsURL: " + bandsInTownURL);
+    case 'concert-this':
 
         getBandsInTown();
         break;
 
     // --- spotify
-    case "spotify-this-song":
+    case 'spotify-this-song':
 
         getSpotify();
         break;
 
 
     // --- omdb
-    case "movie-this":
-        omdbURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + searchTerm;
-        console.log("omdbURL: " + omdbURL);
+    case 'movie-this':
+        omdbURL = 'http://www.omdbapi.com/?apikey=trilogy&t=' + searchTerm;
+        console.log('omdbURL: ' + omdbURL);
 
         getOmdb();
         break;
 
     // --- read random.txt
-    case "do-what-it-says":
+    case 'do-what-it-says':
         doWhatItSays();
         break;
 
-    default: console.log("invalid choice");
+    default: console.log('invalid choice');
+}
+
+
+// ========== FUNCTIONS: switch statement cases ==========
+
+function getBandsInTown() {
+    axios.get('https://rest.bandsintown.com/artists/' + searchTerm + '/events?app_id=codingbootcamp')
+        .then(
+            function (response) {
+
+                // save date to variable & format with moment.js
+                var date = response.data[0].datetime;
+                var formattedDate = moment(date).format('MM/DD/YYYY');
+
+                console.log('Venue Name: ' + response.data[0].venue.name);
+                console.log('Venue Location: ' + response.data[0].venue.city);
+                console.log('Venue Date: ' + formattedDate);
+            });
+}
